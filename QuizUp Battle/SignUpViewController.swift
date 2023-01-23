857b7c66a1b824cd5c15a99bac859bce6ffeadd5
emreozkok <emreozkok88@gmail.com>
2023-01-23 15:49:12 +0300

    iqkeyboard implementation

--- QuizUp Battle/SignUpViewController.swift
+++ QuizUp Battle/SignUpViewController.swift
@@ -19,39 +19,7 @@ class SignUpViewController: UIViewController {
         emailTextField.layer.cornerRadius = 16
         emailTextField.layer.masksToBounds = true
         hideKeyboardWhenTappedAround()
-        
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardApperence(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDisappear(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    var isExpand:Bool = false
-    
-    @objc func keyboardApperence(notification: NSNotification){
-        if !isExpand{
-            if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
-                let keyboardHeight = keyboardFrame.cgRectValue.height
-                self.scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.scrollView.frame.height + keyboardHeight)
-            }
-            else{
-                self.scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.scrollView.frame.height + 250)
-            }
-            isExpand = true
         }
-    }
-    @objc func keyboardDisappear(notification: NSNotification){
-        if isExpand{
-            if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
-                let keyboardHeight = keyboardFrame.cgRectValue.height
-                self.scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.scrollView.frame.height - keyboardHeight)
-            }
-            else{
-                self.scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.scrollView.frame.height - 250)
-            }
-            isExpand = false
-        }
-    }
-    
 }
 
 // Put this piece of code anywhere you like
@@ -61,7 +29,7 @@ extension UIViewController {
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
-    
+
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
